3106c9675d143ed6a153b25eaca175335ac5eaa4
D-hash-code <73314268+D-hash-code@users.noreply.github.com>
2022-10-19 18:28:10 +0100

    cache it bro

--- pages/api/diwali.js
+++ pages/api/diwali.js
@@ -1,10 +1,40 @@
 import { connectGoogleSheet } from "../../lib/connectGoogleSheets";
 
-export default async function handler(req, res) {
-    const doc = await connectGoogleSheet();
-    const sheet = await doc.sheetsByIndex[0];
-    const rows = await sheet.getRows();
+import cacheData from "memory-cache";
 
-    res.status(200).json(rows.map((row) => row._rawData));
+async function fetchWithCache(key) {
+    const value = cacheData.get(key);
+    if (value) {
+        return value;
+    } else {
+        const doc = await connectGoogleSheet();
+        const sheet = await doc.sheetsByIndex[0];
+        const rows = await sheet.getRows();
+        let data = {}
+        rows.forEach((row) => {
+            const centerFirstLetter = row.centreName[0].toUpperCase();
+                const rowItems = {
+                  centreName: row?.centreName ? row.centreName : "",
+                  address: row?.address ? row.address : "",
+                  eventOneDate: row?.eventOneDate ? row.eventOneDate : "",
+                  eventOneDetails: row?.eventOneDetails ? row.eventOneDetails : "",
+                  eventTwoDate: row?.eventTwoDate ? row.eventTwoDate : "",
+                  eventTwoDetails: row?.eventTwoDetails ? row.eventTwoDetails : "",
+                };
+            if (!data[centerFirstLetter]) {
+                data[centerFirstLetter] = [];
+            }
+            data[centerFirstLetter].push(rowItems);
+        });
+        const minutes = 1;
+        cacheData.put(key, data, 1000 * 60 * minutes);
+        return data;
+    }
+}
+
+export default async function handler(req, res) {
+
+    const data = await fetchWithCache("diwali");
+    res.status(200).json(data);
 }
 
